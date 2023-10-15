81870a70ea8223860034f3ba96d5b9160bf68e89
Šimon Žanta <simon.zanta@protonmail.com>
2023-10-15 20:55:45 +0200

    feat(Projekt1): add snapping for every 90 deg

--- Projekt1/src/Canvas.java
+++ Projekt1/src/Canvas.java
@@ -27,6 +27,7 @@ public class Canvas{
     private ArrayList<Line> lines = new ArrayList<>();
 
     Boolean editLine = false;
+    boolean snapToGrid = false;
 
 
     public Canvas(int width, int height) {
@@ -67,8 +68,6 @@ public class Canvas{
                         panel.repaint();
                         lines.clear();
                         break;
-                }
-                switch(e.getKeyCode()){
                     case KeyEvent.VK_E:
                         if(editLine == false && lines.size() > 0){
                             System.out.println("edit is true");
@@ -76,6 +75,16 @@ public class Canvas{
                         }else{
                             System.out.println("edit is false");
                             editLine = false;
+                        }
+                        break;
+                    case KeyEvent.VK_SHIFT:
+
+                        if(snapToGrid == false){
+                            System.out.println("snap enabled");
+                            snapToGrid = true;
+                        }else{
+                            System.out.println("snap disabled");
+                            snapToGrid = false;
                         }
                         break;
                 }
@@ -107,13 +116,18 @@ public class Canvas{
 
             @Override
             public void mouseReleased(MouseEvent e){
+                Point end = new Point(e.getX(), e.getY());
+                Line line = new Line(start, end);
 
-                    Point end = new Point(e.getX(), e.getY());
-                    Line line = new Line(start, end);
+                if(snapToGrid == true){
+                    Point x = returnAngledPoint(line);
+                    end = x;
+                    line = new Line(start, end);
+                }
 
-                    lines.add(line);
+                lines.add(line);
 
-                    lineRaster.redrawLines(img);
+                lineRaster.redrawLines(img);
             }
         });
 
@@ -123,14 +137,50 @@ public class Canvas{
             @Override
             public void mouseDragged(MouseEvent e){
                     img.clear(0x000000);
-
                     Point end = new Point(e.getX(), e.getY());
                     Line line = new Line(start, end);
+
+                    if(snapToGrid == true){
+                        Point x = returnAngledPoint(line);
+                        end = x;
+                        line = new Line(start, end);
+                    }
+
                     lineRaster.drawLine(img, line, 8);
 
                     lineRaster.redrawLines(img);
             }
         });
+    }
+
+    public Point returnAngledPoint(Line line) {
+        int x1 = line.getFirst().x;
+        int x2 = line.getSecond().x;
+        int y1 = line.getFirst().y;
+        int y2 = line.getSecond().y;
+
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+
+        double angle = Math.atan2(dy, dx);
+        double angleDeg = angle * 180 / Math.PI;
+
+        int angleDegRounded = (int) Math.round((angleDeg / 90)) * 90;
+
+        System.out.println(angleDegRounded);
+
+        switch (angleDegRounded){
+            case 90:
+                return new Point(x1, y2);
+            case -90:
+                return new Point(x1, y2);
+            case 0:
+                return new Point(x2, y1);
+            case 180:
+                return new Point(x2, y1);
+        }
+
+        return new Point(x2, y2);
     }
 
     public static void main(String[] args) {
