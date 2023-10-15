4999ae883aa506599a50930b9e49d01b7b2098c4
Šimon Žanta <simon.zanta@protonmail.com>
2023-10-16 00:42:15 +0200

    refactor(Projekt1): move returnnAngledPoint method to line model

--- Projekt1/src/Canvas.java
+++ Projekt1/src/Canvas.java
@@ -120,7 +120,7 @@ public class Canvas{
                 Line line = new Line(start, end);
 
                 if(snapToGrid == true){
-                    Point x = returnAngledPoint(line);
+                    Point x = line.returnAngledPoint();
                     end = x;
                     line = new Line(start, end);
                 }
@@ -141,7 +141,7 @@ public class Canvas{
                     Line line = new Line(start, end);
 
                     if(snapToGrid == true){
-                        Point x = returnAngledPoint(line);
+                        Point x = line.returnAngledPoint();
                         end = x;
                         line = new Line(start, end);
                     }
@@ -151,63 +151,6 @@ public class Canvas{
                     lineRaster.redrawLines(img);
             }
         });
-    }
-
-    public Point returnAngledPoint(Line line) {
-        int x1 = line.getFirst().x;
-        int x2 = line.getSecond().x;
-        int y1 = line.getFirst().y;
-        int y2 = line.getSecond().y;
-
-        int dx = x2 - x1;
-        int dy = y2 - y1;
-
-
-        double angle = Math.toDegrees(Math.atan2(dy, dx));
-
-        if(angle < 0){
-            angle += 360;
-        }
-
-        int angleDegRounded = (int) Math.round((angle / 45)) * 45;
-
-        int length = Math.max(y1, y2) - Math.min(y1, y2);
-
-        double c = (length * (Math.sqrt(2)/2));
-        double b = (c / (Math.sqrt(2)/2));
-        int x, y;
-        switch (angleDegRounded){
-            case 90:
-                return new Point(x1, y2);
-            case 270:
-                return new Point(x1, y2);
-            case 0:
-                return new Point(x2, y1);
-            case 180:
-                return new Point(x2, y1);
-            case 315:
-                x = (int) (x1 + b);
-                y = y1 - length;
-
-                return new Point(x, y);
-            case 225:
-                x = (int) (x1 - b);
-                y = y1 - length;
-
-                return new Point(x, y);
-            case 135:
-                x = (int) (x1 - b);
-                y = y1 + length;
-
-                return new Point(x, y);
-            case 45:
-                x = (int) (x1 + b);
-                y = y1 + length;
-
-                return new Point(x, y);
-        }
-
-        return new Point(x2, y2);
     }
 
     public static void main(String[] args) {
--- Projekt1/src/model/Line.java
+++ Projekt1/src/model/Line.java
@@ -26,4 +26,60 @@ public class Line {
     public void setSecond(Point second) {
         this.second = second;
     }
+    public Point returnAngledPoint() {
+        int x1 = this.getFirst().x;
+        int x2 = this.getSecond().x;
+        int y1 = this.getFirst().y;
+        int y2 = this.getSecond().y;
+
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+
+
+        double angle = Math.toDegrees(Math.atan2(dy, dx));
+
+        if(angle < 0){
+            angle += 360;
+        }
+
+        int angleDegRounded = (int) Math.round((angle / 45)) * 45;
+
+        int length = Math.max(y1, y2) - Math.min(y1, y2);
+
+        double c = (length * (Math.sqrt(2)/2));
+        double b = (c / (Math.sqrt(2)/2));
+        int x, y;
+        switch (angleDegRounded){
+            case 90:
+                return new Point(x1, y2);
+            case 270:
+                return new Point(x1, y2);
+            case 0:
+                return new Point(x2, y1);
+            case 180:
+                return new Point(x2, y1);
+            case 315:
+                x = (int) (x1 + b);
+                y = y1 - length;
+
+                return new Point(x, y);
+            case 225:
+                x = (int) (x1 - b);
+                y = y1 - length;
+
+                return new Point(x, y);
+            case 135:
+                x = (int) (x1 - b);
+                y = y1 + length;
+
+                return new Point(x, y);
+            case 45:
+                x = (int) (x1 + b);
+                y = y1 + length;
+
+                return new Point(x, y);
+        }
+
+        return new Point(x2, y2);
+    }
 }
