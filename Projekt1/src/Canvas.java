7ef0932d9491aa90af92ef8967200b59b83fc4c5
Šimon Žanta <simon.zanta@protonmail.com>
2023-11-05 16:22:56 +0100

    refactor(Canvas): redo switch to buttons

--- Projekt1/src/Canvas.java
+++ Projekt1/src/Canvas.java
@@ -1,54 +1,54 @@
+import Utils.BooleanUtil;
+import Utils.ButtonUtil;
+import Utils.ColorUtil;
+import model.Line;
+import model.Point;
+import model.Polygon;
+import rasterData.RasterBI;
+import rasterOp.Fill.SeedFill;
+import rasterOp.LineRasterizers.LineRasterizerBresenham;
+import rasterOp.LineRasterizers.Liner;
+import rasterOp.PolygonRasterizers.PolygonRasterizer;
+
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Optional;
 
-import rasterOp.Fill.SeedFill;
-import rasterOp.LineRasterizerBresenham;
-import rasterOp.LineRasterizerMidpoint;
-import rasterData.RasterBI;
-import model.Point;
-import model.Line;
-import rasterOp.Liner;
-import model.Polygon;
-import rasterOp.PolygonRasterizer;
+public class Canvas {
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
-
-public class Canvas{
-
-    private JFrame frame;
-    private JPanel panel;
-    private RasterBI img;
-
-    private int startX, startY;
-    private Point start;
-
-    private ArrayList<Line> lines = new ArrayList<>();
-
+    private final JPanel panel;
+    private final RasterBI img;
+    private final ArrayList<Line> lines = new ArrayList<>();
     boolean editLine = false;
     boolean snapToGrid = false;
     boolean createPoligon = false;
     boolean fillPlane = false;
-
     ArrayList<Point> polygonArray = new ArrayList<>();
     Polygon polygon = new Polygon(polygonArray);
+    BooleanUtil booleanUtil = new BooleanUtil();
+    ButtonUtil buttonUtil = new ButtonUtil();
+    private int startX, startY;
+    private Point start;
 
     public Canvas(int width, int height) {
-        frame = new JFrame();
+        JFrame frame = new JFrame();
 
         frame.setLayout(new BorderLayout());
         frame.setTitle("UHK FIM PGRF : " + this.getClass().getName());
         frame.setResizable(false);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+
         // set pixel settings, witdth, height and rgb
-        img = new RasterBI(width, height);
+        img = new RasterBI(width - 150, height);
 
         panel = new JPanel() {
+            @Serial
             private static final long serialVersionUID = 1L;
 
             // inicialize default graphics in panel
@@ -59,6 +59,8 @@ public class Canvas{
             }
         };
         panel.setPreferredSize(new Dimension(width, height));
+        panel.setLayout(null);
+
         frame.add(panel, BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
@@ -67,55 +69,38 @@ public class Canvas{
         Liner lineRaster = new LineRasterizerBresenham();
         SeedFill seedFill = new SeedFill();
 
-        panel.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                switch(e.getKeyCode()){
-                    case KeyEvent.VK_C:
-                        img.clear(0x000000);
-                        panel.repaint();
-                        lines.clear();
-                        break;
-                    case KeyEvent.VK_E:
-                        if(editLine == false && lines.size() > 0){
-                            System.out.println("edit is true");
-                            editLine = true;
-                        }else{
-                            System.out.println("edit is false");
-                            editLine = false;
-                        }
-                        break;
-                    case KeyEvent.VK_SHIFT:
+        //inicializing all buttons
+        JButton clearButton = buttonUtil.createNewButton("clear", width, 20);
+        JButton editLineButton = buttonUtil.createNewButton("edit line", width, 20);
+        JButton snapButton = buttonUtil.createNewButton("snap tool", width, 20);
+        JButton poligonButton = buttonUtil.createNewButton("poligon", width, 20);
+        JButton fillButton = buttonUtil.createNewButton("fill", width, 20);
 
-                        if(snapToGrid == false){
-                            System.out.println("snap enabled");
-                            snapToGrid = true;
-                        }else{
-                            System.out.println("snap disabled");
-                            snapToGrid = false;
-                        }
-                        break;
-                    case KeyEvent.VK_P:
+        panel.add(clearButton);
+        panel.add(editLineButton);
+        panel.add(snapButton);
+        panel.add(poligonButton);
+        panel.add(fillButton);
 
-                        if(createPoligon == false){
-                            System.out.println("Poligon enabled");
-                            createPoligon = true;
-                        }else{
-                            System.out.println("Poligon disabled");
-                            createPoligon = false;
-                        }
-                        break;
-                    case KeyEvent.VK_F:
-                        if(fillPlane == false){
-                            System.out.println("fillPlane enabled");
-                            fillPlane = true;
-                        }else{
-                            System.out.println("fillPlane disabled");
-                            fillPlane = false;
-                        }
-                        break;
-                }
-            }
+        clearButton.addActionListener(e -> {
+            img.clear(0x000000);
+            panel.repaint();
+            lines.clear();
+            polygonArray.clear();
+        });
+
+        editLineButton.addActionListener(e -> editLine = !editLine);
+
+        snapButton.addActionListener(e -> snapToGrid = booleanUtil.switchValue(snapToGrid));
+
+        poligonButton.addActionListener(e -> {
+            createPoligon = booleanUtil.switchValue(createPoligon);
+            fillPlane = false;
+        });
+
+        fillButton.addActionListener(e -> {
+            fillPlane = booleanUtil.switchValue(fillPlane);
+            createPoligon = false;
         });
 
         PolygonRasterizer polygonRasterizer = new PolygonRasterizer();
@@ -123,8 +108,8 @@ public class Canvas{
         panel.addMouseListener(new MouseAdapter() {
 
             @Override
-            public void mouseClicked(MouseEvent e){
-                if(createPoligon){
+            public void mouseClicked(MouseEvent e) {
+                if (createPoligon) {
                     img.clear(0x000000);
                     lineRaster.redrawLines(img, lines);
                     startX = e.getX();
@@ -137,23 +122,27 @@ public class Canvas{
                     polygonRasterizer.drawPolygon(img, polygon);
                     panel.repaint();
                 }
-                if(fillPlane){
-                    seedFill.fill4(img, panel, new Point(e.getX(), e.getY()));
+                if (fillPlane) {
+                    ColorUtil colorUtil = new ColorUtil();
+                    int randomColor = colorUtil.getRandomColor();
+
+                    seedFill.fill4(img, panel, new Point(e.getX(), e.getY()), randomColor);
 
                     panel.repaint();
                 }
             }
+
             @Override
             public void mousePressed(MouseEvent e) {
-                    startX = e.getX();
-                    startY = e.getY();
+                startX = e.getX();
+                startY = e.getY();
 
-                    start = new Point(startX, startY);
+                start = new Point(startX, startY);
 
-                if(editLine){
-                    Optional<Point> point = start.getClosestEndpointInRadius(img, lineRaster);
+                if (editLine) {
+                    Optional<Point> point = start.getClosestEndpointInRadius(img, lineRaster, lines);
 
-                    if(point.isPresent()){
+                    if (point.isPresent()) {
                         Point startPoint = point.get();
 
                         int x = startPoint.x;
@@ -165,14 +154,13 @@ public class Canvas{
             }
 
             @Override
-            public void mouseReleased(MouseEvent e){
-                if(!createPoligon){
+            public void mouseReleased(MouseEvent e) {
+                if (!createPoligon) {
                     Point end = new Point(e.getX(), e.getY());
                     Line line = new Line(start, end);
 
-                    if(snapToGrid == true){
-                        Point x = line.returnAngledPoint();
-                        end = x;
+                    if (snapToGrid) {
+                        end = line.returnAngledPoint();
                         line = new Line(start, end);
                     }
 
@@ -187,19 +175,16 @@ public class Canvas{
         });
 
         panel.addMouseMotionListener(new MouseMotionAdapter() {
-
-
             @Override
-            public void mouseDragged(MouseEvent e){
+            public void mouseDragged(MouseEvent e) {
 
-                if(!createPoligon){
+                if (!createPoligon) {
                     img.clear(0x000000);
                     Point end = new Point(e.getX(), e.getY());
                     Line line = new Line(start, end);
 
-                    if(snapToGrid == true){
-                        Point x = line.returnAngledPoint();
-                        end = x;
+                    if (snapToGrid) {
+                        end = line.returnAngledPoint();
                         line = new Line(start, end);
                     }
 
