c63c2b795ba01e4036408618be760496f6367468
Šimon Žanta <simon.zanta@protonmail.com>
2023-11-14 18:54:56 +0100

    feat(CalcUtils): add buble sort algorithm

--- Projekt1/src/Utils/CalcUtils.java
+++ Projekt1/src/Utils/CalcUtils.java
@@ -3,6 +3,8 @@ package Utils;
 import model.Line;
 import model.Point;
 
+import java.util.ArrayList;
+
 public class CalcUtils {
     public Line orientateTwoPointsToLine(Point point1, Point point2){
         int x1 = point1.x;
@@ -19,5 +21,19 @@ public class CalcUtils {
         }
 
         return new Line(x1-1, y1-1, x2, y2);
+    }
+
+    public static ArrayList<Integer> BubbleSort(ArrayList<Integer> intersections){
+        for (int j = 0; j < intersections.size()-1; j++) {
+            for (int k = 0; k < intersections.size()-j-1; k++) {
+                if (intersections.get(k) > intersections.get(k+1)) {
+                    //swap
+                    int temp = intersections.get(k);
+                    intersections.set(k, intersections.get(k+1));
+                    intersections.set(k+1, temp);
+                }
+            }
+        }
+        return intersections;
     }
 }
