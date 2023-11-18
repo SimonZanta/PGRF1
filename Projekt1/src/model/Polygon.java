937b1ff530f6949dbe177ade3b68ddad4b40bc6b
Šimon Žanta <simon.zanta@protonmail.com>
2023-11-18 17:05:36 +0100

    feat(Polygon): create method for returning Polygon as list of edges

--- Projekt1/src/model/Polygon.java
+++ Projekt1/src/model/Polygon.java
@@ -13,8 +13,26 @@ public class Polygon {
     public void addPoint(Point point){
         vertexArray.add(point);
     }
-
     public int getSize(){
         return this.vertexArray.size();
+    }
+    public ArrayList<Point> getVertexArray(){
+        return this.vertexArray;
+    }
+    public ArrayList<Line> getEdges(){
+        ArrayList<Line> edges = new ArrayList<>();
+        for(int point = 0; point < vertexArray.size(); point++){
+
+            int indexA = point;
+            int indexB = point + 1;
+            if (indexB == vertexArray.size()){
+                indexB = 0;
+            }
+
+            Line edge = new Line(vertexArray.get(indexA), vertexArray.get(indexB));
+
+            edges.add(edge);
+        }
+        return edges;
     }
 }
